4750fc5ba0d6bc49d207def05e6b0e39b5b8009c
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-02-06 21:24:09 +0530

    [Aniket] Adding User registration using Testcases and exception program

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UserRegistration;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("User Details")]
+        public void USerDetails()
+        {
+            bool expected = true;
+            string firstName = "Sahil";
+            string lastName = "Gaikwad";
+            string emailID = "sahil122@ggmail.com";
+            string monum = "91 8190538095";
+            string password = "Beif@du1";
+            UserRegistration1 pattern = new UserRegistration1();
+            Assert.AreEqual(expected, pattern.ValidateFName(firstName));
+            Assert.AreEqual(expected, pattern.ValidateLName(lastName));
+            Assert.AreEqual(expected, pattern.ValidateEmail(emailID));
+            Assert.AreEqual(expected, pattern.ValidateMobileNumber(monum));
+            Assert.AreEqual(expected, pattern.ValidatePasswordRule4(password));
+        }
+        //testing all emails samples
+        [TestMethod]
+        [TestCategory("Sample Emails")]
+        [DataRow("abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com")]
+        public void ValidateEmailId(string email)
+        {
+            bool expected = true;
+            UserRegistration1 pattern = new UserRegistration1();
+            Assert.AreEqual(expected, pattern.ValidateEmail(email));
+        }
+    }
+}
